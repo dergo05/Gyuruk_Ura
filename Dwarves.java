5b7d6a2fe0cd9c5dd539ae1bcdbf757c5c0da3ba
dukkona <dukkon.a@gmail.com>
2014-03-20 18:15:31 +0100

    Update Dwarves.java

--- Dwarves.java
+++ Dwarves.java
@@ -1,4 +1,3 @@
-
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.9B748F6A-139B-940B-12E0-9E2FBC2A8118]
 // </editor-fold> 
@@ -6,7 +5,11 @@ public class Dwarves extends Enemies {
 
     public void doTick(Timer t) {
         throw new UnsupportedOperationException("Not supported yet.");
+    
+    //komment
     }
+    
+    
 
 }
 
